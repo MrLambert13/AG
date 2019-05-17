ade4afb5b83eae725fe9b1019a4bb2c5e84919ed
admkvv <admkvv@gmail.com>
2019-05-17 10:30:26 +0300

    первый пуш

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=192.168.253.3:3306;dbname=autogigant',
+    'username' => 'foo',
+    'password' => 'PPPppp!2345))',
+    'charset' => 'utf8',
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
