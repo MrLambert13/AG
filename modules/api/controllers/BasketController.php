fbdab04d29674111a8e3098f4e0f5b7131b9b4e7
MrLambert13 <Lam_13@mail.ru>
2019-06-15 20:34:31 +0300

    add basket filter

--- modules/api/controllers/BasketController.php
+++ modules/api/controllers/BasketController.php
@@ -3,6 +3,7 @@
 namespace app\modules\api\controllers;
 
 use app\models\Basket;
+use Yii;
 use yii\rest\ActiveController;
 
 /**
@@ -11,5 +12,46 @@ use yii\rest\ActiveController;
 class BasketController extends ActiveController
 {
     public $modelClass = Basket::class;
+    private $params;
+
+
+    public function behaviors()
+    {
+        parent::behaviors();
+
+        $this->initParams();
+        return parent::behaviors();
+    }
+
+    private function initParams()
+    {
+        $this->params = Yii::$app->request->bodyParams;
+    }
+
+    public function actions()
+    {
+        $actions = parent::actions();
+
+        $actions['index']['prepareDataProvider'] = [$this, 'prepareDataProvider'];
+
+        return $actions;
+    }
+
+    public function prepareDataProvider()
+    {
+        $basket = Basket::find()->byUser($this->params['idUser']);
+        $result = $basket
+            ? [
+                'success' => 1,
+                'message' => 'Baskets finded',
+                'payload' => $basket,
+            ]
+            : [
+                'success' => 0,
+                'message' => 'Baskets is not finded',
+                'code' => 'error_find',
+            ];
+        return $result;
+    }
 
 }
